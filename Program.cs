d0e67bc68a84b02980383ddc83ff69b6fab085dd
hsr1991 <hsr1991@hotmail.co.uk>
2021-02-22 14:24:17 +0000

    user input done rules work but not simplest way

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FizzbuzzC
 {
@@ -6,7 +7,60 @@ namespace FizzbuzzC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a number:");
+    int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                List<String> output = new List<String> { };
+                if (i % 3 == 0)
+                {
+                    output.Add("Fizz");
+                }
+                if (i%13 == 0) {
+                    output.Add("Fezz");
+                }
+                if (i % 5 == 0)
+                {
+                    output.Add("Buzz");
+                }
+                  if (i%7 == 0)
+                {
+                    output.Add("Bang");
+                }
+                  if (i%11 == 0) 
+                  {
+                      List<String> output2 = new List<String> { "Bong"};
+                      Console.WriteLine(String.Join("", output2));
+                      
+                  }
+                  if (i%11 == 0 && i%13 == 0) {
+                    List<String> output3 = new List<String> { "FezzBong"};
+                      Console.WriteLine(String.Join("", output3));
+                  }
+                {
+                        if (i%17 == 0) {
+                      output.Reverse();
+                  }
+
+                }
+                if (output.Count == 0)
+                {
+                    Console.WriteLine(i);
+                }
+              
+                else
+                {
+                    Console.WriteLine(String.Join("", output));
+                    
+                }
+
+
+
+
+
+            }
+
+
         }
     }
 }
