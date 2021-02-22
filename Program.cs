3c51bf61478d33e0d2c6a6fe2d874df03b5ee9a6
hsr1991 <hsr1991@hotmail.co.uk>
2021-02-22 15:29:33 +0000

    moved fezzbong and bong to the start

--- Program.cs
+++ Program.cs
@@ -7,56 +7,53 @@ namespace FizzbuzzC
     {
         static void Main(string[] args)
         {
+            // Console.WriteLine(args.Length);
+            // foreach (var arg in args);
+            // Console.WriteLine(args);
+
             Console.WriteLine("Please enter a number:");
-    int number = Convert.ToInt32(Console.ReadLine());
+            int number = Convert.ToInt32(Console.ReadLine());
             for (int i = 1; i <= number; i++)
             {
                 List<String> output = new List<String> { };
+                if (i % 11 == 0 && i % 13 == 0)
+                {
+                    Console.WriteLine("FezzBong");
+                    continue;
+                }
+                if (i % 11 == 0)
+                {
+                    Console.WriteLine("Bong");
+                    continue;
+                }
                 if (i % 3 == 0)
                 {
                     output.Add("Fizz");
                 }
-                if (i%13 == 0) {
+                if (i % 13 == 0)
+                {
                     output.Add("Fezz");
                 }
                 if (i % 5 == 0)
                 {
                     output.Add("Buzz");
                 }
-                  if (i%7 == 0)
+                if (i % 7 == 0)
                 {
                     output.Add("Bang");
                 }
-                  if (i%11 == 0) 
-                  {
-                      List<String> output2 = new List<String> { "Bong"};
-                      Console.WriteLine(String.Join("", output2));
-                      
-                  }
-                  if (i%11 == 0 && i%13 == 0) {
-                    List<String> output3 = new List<String> { "FezzBong"};
-                      Console.WriteLine(String.Join("", output3));
-                  }
+                if (i % 17 == 0)
                 {
-                        if (i%17 == 0) {
-                      output.Reverse();
-                  }
-
+                    output.Reverse();
                 }
                 if (output.Count == 0)
                 {
                     Console.WriteLine(i);
-                }
-              
-                else
+                } else
                 {
                     Console.WriteLine(String.Join("", output));
-                    
+
                 }
-
-
-
-
 
             }
 
